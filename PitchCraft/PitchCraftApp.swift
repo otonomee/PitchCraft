04ad2b594d1ca126eb0c0c003e8f9ae59b70cd87
Austin Allen <austinrallen96@gmail.com>
2024-08-13 19:10:01 -0700

    Initial Commit

---
+++ PitchCraft/PitchCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchCraftApp.swift
+//  PitchCraft
+//
+//  Created by Austin Allen on 2024-08-13.
+//
+
+import SwiftUI
+
+@main
+struct PitchCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
